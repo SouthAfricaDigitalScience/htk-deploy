d2db621de172098b0476e88b785d2fb04ebeba55
Bruce Becker <brucellino@gmail.com>
2016-05-03 15:22:04 +0200

    Fixed #95. Fixed  #96

--- check-build-3.4.1.sh
+++ check-build-3.4.1.sh
@@ -17,6 +17,7 @@ echo "Installing to ${SOFT_DIR}"
 # Build 30 died with mkdir: cannot create directory ‘/apprepo/generic/u1404/x86_64/htk/3.4.1/bin’: No such file or directory
 # so, apparently, we need to make this dir ourselves - smh
 mkdir -vp ${SOFT_DIR}
+cd ${WORKSPACE}/htk
 make install
 echo "Installing HDecode"
 make install-hdecode
